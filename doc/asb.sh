c1b59ef6eaec693709b1544618e53145bd62f378
malcolm north <malcolm@thenorths.me>
2025-02-28 14:45:59 +0000

    Work-DocUpdated-20250228

---
+++ doc/asb.sh
@@ -0,0 +1,24 @@
+cat << 'EOF' > asb.adoc
+= ASB Protocol Design Notes
+
+== Foundational Influences
+include::introduction.adoc[]
+
+== Physical Layer
+include::physical_layer.adoc[]
+
+== Data Link Layer
+include::data_link.adoc[]
+
+== Frame Format
+include::frame_format.adoc[]
+
+== Bus Protocol
+include::bus_protocol.adoc[]
+
+== Transport Protocol
+include::transport.adoc[]
+
+== Performance Analysis
+include::performance.adoc[]
+EOF
